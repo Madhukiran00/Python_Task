927ccc13491b26e7826632243816b3891279c518
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-04-20 16:24:34 +0530

    Create 14-04-25-Task1.py

---
+++ 14-04-25-Task1.py
@@ -0,0 +1,29 @@
+
+#GLobal 
+x = 10
+def change():
+    global x
+    x = 20
+change()
+print(x)  
+
+#Local
+def change():
+    x = 20
+    print(x)
+change()
+
+#enclosed
+
+def outer():
+    x = 20
+    def inner():
+        print(x)
+    inner()
+outer()
+
+#built-in
+def change():
+    x = 20
+    print(x)
+change()
