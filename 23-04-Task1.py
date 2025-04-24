8248141b14766b44b5faee84d3ddecae54969e6d
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-04-24 23:28:59 +0530

    Create 23-04-Task1.py

---
+++ 23-04-Task1.py
@@ -0,0 +1,27 @@
+
+
+class Laptop:
+    
+    def __init__(self,brand,model ,processor ,ram,storage):
+        self.brand=brand
+        self.model=model
+        self.processor=processor
+        self.ram=ram
+        self.storage=storage
+      
+    def about(self):
+        
+        return f"self.brand,self.model,self.processor,self.ram,self.storage"
+    
+    
+laptop1=Laptop("HP","OMEN 16","Intel Core i7","16GB DDR5","1TB SSD")
+laptop2=Laptop("HP", "OMEN","AMD Rtzen","16GB","1TB")
+laptop3=Laptop("Lenovo","IdeaPad","Intel Core Ultra 9","16GB","1TB")
+laptop4=Laptop("Asus","Zen Book 14 OLED","Intel Core Ultra 7","16GB","1TB")
+laptop5=Laptop("Dell","Inspiron 14","Intel core i5","*GB","512")
+laptop6=Laptop("Lenovo","ThinkPad","Intel core i3","8GB","512 GB ")
+laptop7=Laptop("Microsoft","Surface Laptop","Intel core i7","8GB","512 GB")
+laptop8=Laptop("Aces","Aspire 5","Ryzen 5","8GB","512GB")
+
+         
+
