75f05f61a2659a3743686ba79c1d141b59a394c4
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-04-26 14:26:42 +0530

    Create Task1.py

---
+++ 22-04-25/Task1.py
@@ -0,0 +1,27 @@
+class Classone:
+    var1="class1"
+    def fun1(self):
+        print(f"This is first {self.var1}")
+        
+obj1=Classone()
+obj1.fun1()
+
+
+class Classtwo:
+    var2="class2"
+    def fun1(self):
+        print(f"This is secound {self.var2}")
+        
+obj1=Classtwo()
+obj1.fun1()
+
+class Classthree:
+    var3="class3"
+    def fun1(self):
+        print(f"This is three {self.var4}")
+        
+obj1=Classthree()
+obj1.fun1()
+
+
+
