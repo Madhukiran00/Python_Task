a3e6ae0867aa844ab90dbd7804925db54da30927
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-05-07 11:23:19 +0530

    Update Task-3.py

--- 06-05-2025/Task-3.py
+++ 06-05-2025/Task-3.py
@@ -2,9 +2,6 @@ n=5
 c=True
 for i in range(n,0,-1):
     for j in range(1,3):
-        if n%2!=0 and c==True:
-            print("*"*5)
-            c==False
-        else:
-           print("*",end=" ")
+        if n%2!=0 ceil(n/2):
+        print("*",end=" ")
     print("\n")
