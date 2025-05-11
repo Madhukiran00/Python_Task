8f1f6052e253a8a67d373421e6c38a02e9a45ff8
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-05-11 17:40:07 +0530

    Update Task-1.py

--- 28-04-25/Task-1.py
+++ 28-04-25/Task-1.py
@@ -1,5 +1,16 @@
-string="aabbbc"  #a2b3c1
+string="aabbbc"  
+temp=string[0]
+count=0
 res=""
 for i in range(len(string)):
+    if string[i]==temp:
+        count+=1
+    else:
+        res+=temp+str(count)
+        temp=string[i]
+        count=1
+res+=temp+str(count)
+print(res)      
+    
 
     
