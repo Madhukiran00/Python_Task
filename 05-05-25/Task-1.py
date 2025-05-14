456fc3fe99699d7c19dc07db4d3083fbfc92f399
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-05-14 16:02:58 +0530

    Update Task-1.py

--- 05-05-25/Task-1.py
+++ 05-05-25/Task-1.py
@@ -1,10 +1,19 @@
 
-import math
+# import math
+# n=5
+# for row in range(1,n+1):
+#     for col in range(1,n+1):
+#         if row==1 or row==5 or col==1 or col==5 or  (row==math.ceil(n/2) and col==math.ceil(n/2)):
+#              print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
+#     print()
+
 n=5
 for row in range(1,n+1):
-    for col in range(1,n+1):
-        if row==1 or row==5 or col==1 or col==5 or  (row==math.ceil(n/2) and col==math.ceil(n/2)):
-             print("*",end=" ")
+    for col in range(1,(n+n)+1):
+        if row==n or  row+(n-1)==n:
+            print("*",end=" ")
         else:
             print(" ",end=" ")
     print()
