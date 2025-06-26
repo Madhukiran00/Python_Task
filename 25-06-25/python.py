a579b4f8086aca53dbaf6023421881619285c934
PILLI MADHUKIRAN <162177926+Madhukiran00@users.noreply.github.com>
2025-06-26 09:03:13 +0530

    Update python.py

--- 25-06-25/python.py
+++ 25-06-25/python.py
@@ -1,19 +1,10 @@
-# file=open("./File/madhu.txt","r")
-# res=''
-# for char in file.readlines():
-#     res+=char[0]
-# print(res)
+file=open("./File/madhu.txt","r")
+res=''
+for char in file.readlines():
+    res+=char[0]
+print(res)
 
-import os
-# if os.path.exists("madhu.txt"):
-#     file=open("madhu.txt","r")
-#     res=''
-#     for char in file.readlines():
-#         res+=char[0]
-#     print(res)
-    
-print(os.getcwd())  # Check current working directory
-print(os.path.exists("madhu.txt"))   
+ 
     
 
     
