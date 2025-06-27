65dadfb1828f8144bc659defb41e2a80d7afd498
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-06-27 21:13:17 +0530

    Update Task-1.py

--- 27-06-25/Task-1.py
+++ 27-06-25/Task-1.py
@@ -1,8 +1,61 @@
 # 1)
-n=5
+# n=5
 
-for i in range(1,n+1):
-    for j in range(n):
-        if i==0 or i==n:
-        print("*",end=" ")
-    print("")
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if i==j or j==1 or j==n:
+#             print("*",end=" ")
+#         else:
+#             print("  ",end="")
+#     print("")
+
+#Output:
+# *       * 
+# * *     * 
+# *   *   * 
+# *     * * 
+# *       * 
+
+#------------------------------------
+
+# n=5
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if  (i+j)==n+1 or i==1 or i==n:
+#             print("*",end=" ")
+#         else:
+#             print("  ",end="")
+#     print("")
+    
+#Output:
+
+#  * * * * * 
+#        *   
+#      *     
+#    *       
+#  * * * * * 
+#---------------------------
+
+# n=5
+# for i in range(1,n+1):
+
+#     for j in range(1,n+1):
+    
+#         if i==1 or i==n or j==(n//2)+1:
+#             print("*",end=" ")
+#         else:
+#             print("  ",end="")
+#     print("")
+    
+# Output:
+#  * * * * * 
+#      *     
+#      *     
+#      *     
+#  * * * * * 
+#----------------------------------------
+
+
+
+
+
