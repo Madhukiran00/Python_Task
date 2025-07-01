545b16ac855ffdf5a34c7ec3985c89c372a7ae08
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-07-01 14:41:42 +0530

    Update Task1.py

--- 01-07-25/Task1.py
+++ 01-07-25/Task1.py
@@ -1,6 +1,10 @@
 from datetime import datetime 
 
-print(datetime.now())
-print(datetime.now().date())
+# print(datetime.now())
+# print(datetime.now().date())
 
 #strptime
+
+print(datetime.strptime("2025-07-01","%Y-%m-%d"))
+
+print(datetime.strptime("01-07-2025","%d-%m-%Y"))
